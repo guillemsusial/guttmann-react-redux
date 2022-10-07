c54898bcbf72c9a4c5b2f0f0f5f662eff342b8ff
guillemsusial <guillemsusial@gmail.com>
2022-10-07 20:54:23 +0200

    dropdown header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,36 @@
-const Header = () => {
-    return (
-        <header id="header" class="">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-6">
-                        buenas tardes
-                    </div>
-                    <div class="col-6">
-                        hola gei
+import React,{ Component } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+
+import user from '../../assets/images/userDefault.png';
+
+class Header extends Component {
+    render(){
+        return (
+            <header>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-6">
+                            <a href="#">
+                                <img src={user}/>
+                            </a>
+                        </div>
+                        <div class="col-6">
+                            <Dropdown>
+                                <Dropdown.Toggle>
+                                
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#">Menu Item</Dropdown.Item>
+                                    <Dropdown.Item href="#">Menu Item</Dropdown.Item>
+                                    <Dropdown.Item href="#">Menu Item</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </header>
-    )
+            </header>
+        );
+    }
 }
 
 export default Header;
