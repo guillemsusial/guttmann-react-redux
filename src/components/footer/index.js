c11a0f8e2d4f226c6ea0f87937b571bbc226e326
Marc <78415669+Marcsegbla@users.noreply.github.com>
2022-10-10 17:31:06 +0200

    enlaces

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,48 +1,47 @@
 import { Component } from "react";
 import './index.sass';
+const imagenes = require.context('/src/assets/images', true);
 
-class footer extends Component {
-    render() {
-        return (
-            <footer>
-                <div class="container-fluid col-12 col-sm-12 pb-5 pt-2 rounded-top m-0 color">
-                    <br/>
+function footer() {
+    return (
+        <footer>
+            <div class="container-fluid col-12 col-sm-12 pb-5 pt-2 rounded-top m-0 color">
+                <br/>
 
-                    {/* Linia 1 */}
-                    <div class="row">
-                        <div class="col-2 col-sm-2 mx-3"></div>
-                        <div class="col-8 col-sm-8 ml-2 mr-0 text-white-50 text-break fw-bold content">
-                            <hr class="bg-light"/>
+                {/* Linia 1 */}
+                <div class="row">
+                    <div class="col-2 col-sm-2 mx-3"></div>
+                    <div class="col-8 col-sm-8 ml-2 mr-0 text-white-50 text-break fw-bold content">
+                        <hr class="bg-light"/>
 
-                            {/* linia divisoria */}
-                            <div class="row">
-                                <div class="col-8 col-sm-8 ml-2 mr-0 text-white-50 text-break fw-bold">
-                                    © 2022 Institut Guttmann <span class="dot"></span><a class="text-white-50 " href="#">Aviso legal</a> <span
-                                    class="dot"></span><a class="text-white-50" href="#">Política de Cookies</a> <span class="dot"></span><a
-                                    class="text-white-50" href="#">Privacidad y Protección de datos</a>
-                                </div>
+                        {/* linia divisoria */}
+                        <div class="row">
+                            <div class="col-8 col-sm-8 ml-2 mr-0 text-break fw-bold enlaces">
+                                © 2022 Institut Guttmann <span class="dot"></span><a class="enlaces" href="#">Aviso legal</a> <span
+                                class="dot"></span><a class="enlaces" href="#">Política de Cookies</a> <span class="dot"></span><a
+                                class="enlaces" href="#">Privacidad y Protección de datos</a>
+                            </div>
 
-                                {/* REDES SOCIALES */}
-                                <div class="col-4 col-sm-4 ml-0 text-white-50  text-break fw-bold text-center">
-                                    <a class="mx-2" href="https://twitter.com/IGuttmann"><img class="wicon "
-                                    src="../../../assets/img/twiter.svg" alt="" srcset=""/></a>
-                                    <a class="mx-2" href="https://www.instagram.com/iguttmann/"><img class="wicon "
-                                    src="../../../assets/img/instagram-gray.svg" alt="" srcset=""/></a>
-                                    <a class="mx-2" href="https://www.facebook.com/Institut.Guttmann"><img class="wicon "
-                                    src="../../../assets/img/facebook-gray.svg" alt="" srcset=""/></a>
-                                    <a class="mx-2"
-                                    href="https://www.linkedin.com/company/institut-guttmann?original_referer=https%3A%2F%2Fwww.guttmann.com%2F"><img
-                                    class="wicon " src="../../../assets/img/linkedin-gray.svg" alt="" srcset=""/></a>
-                                    <a class="mx-2" href="https://www.youtube.com/user/InstitutGuttmann"><img class="wicon "
-                                    src="../../../assets/img/youtube-gray.svg" alt="" srcset=""/></a>
-                                </div>
+                            {/* REDES SOCIALES */}
+                            <div class="col-4 col-sm-4 ml-0 text-white-50  text-break fw-bold text-center">
+                                <a class="mx-2" href="https://twitter.com/IGuttmann"><img class="wicon "
+                                src={imagenes(`./twitter-gray.svg`)} alt="" srcset=""/></a>
+                                <a class="mx-2" href="https://www.instagram.com/iguttmann/"><img class="wicon "
+                                src={imagenes(`./instagram-gray.svg`)} alt="" srcset=""/></a>
+                                <a class="mx-2" href="https://www.facebook.com/Institut.Guttmann"><img class="wicon "
+                                src={imagenes(`./facebook-gray.svg`)} alt="" srcset=""/></a>
+                                <a class="mx-2"
+                                href="https://www.linkedin.com/company/institut-guttmann?original_referer=https%3A%2F%2Fwww.guttmann.com%2F"><img
+                                class="wicon " src={imagenes(`./linkedin-gray.svg`)} alt="" srcset=""/></a>
+                                <a class="mx-2" href="https://www.youtube.com/user/InstitutGuttmann"><img class="wicon "
+                                src={imagenes(`./youtube-gray.svg`)} alt="" srcset=""/></a>
                             </div>
                         </div>
                     </div>
                 </div>
-            </footer>
-        )
-    }
+            </div>
+        </footer>
+    )
 }
 
 export default footer;
