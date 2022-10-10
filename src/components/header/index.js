d8d76149315d3c8031085bb3ae789da9bca04433
guillemsusial <guillemsusial@gmail.com>
2022-10-10 17:29:37 +0200

    header v1

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,36 +1,35 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+import './index.sass'
 
-import user from '../../assets/images/userDefault.png';
+import user from "../../assets/images/userDefault.png";
 
-class Header extends Component {
-    render(){
-        return (
-            <header>
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="#">
-                                <img src={user}/>
-                            </a>
-                        </div>
-                        <div class="col-6">
-                            <Dropdown>
-                                <Dropdown.Toggle>
-                                
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-                                    <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-                                    <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </div>
-                    </div>
-                </div>
-            </header>
-        );
-    }
+function Header() {
+  return (
+    <header>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-6 logo">
+            <a href="#">
+              <img src={user} />
+            </a>
+          </div>
+          <div class="col-6 user">
+            <Dropdown>
+              <Dropdown.Toggle>
+                <img src={user} />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#">Menu Item</Dropdown.Item>
+                <Dropdown.Item href="#">Menu Item</Dropdown.Item>
+                <Dropdown.Item href="#">Menu Item</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
