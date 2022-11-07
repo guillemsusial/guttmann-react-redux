2cce56117f73c4dcb8673278880e4345439c1b5e
guillemsusial <guillemsusial@gmail.com>
2022-11-07 17:25:24 +0100

    routes and start new main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Home from "./pages/home";
+import Homev2 from "./pages/home_v2";
 import User from "./pages/user";
-import Error404 from "./pages/error404";
+import Error404 from "./routes/error404";
+import Error403 from "./routes/error403";
 
 import { Route, Routes } from "react-router-dom"
 
@@ -13,7 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/user" element={<User/>}/>
+        <Route path="/homev2" element={<Homev2/>}/>
+
         <Route path="*" element={<Error404/>} />
+
       </Routes>
     </React.Fragment>
   );
--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,5 +1,3 @@
-import Introduction from "../../components/home/introduction";
-import Categories from "../../components/home/introduction";
 import React, { Component } from "react";
 
 function user() {
